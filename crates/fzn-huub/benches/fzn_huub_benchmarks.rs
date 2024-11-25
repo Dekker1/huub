Dekker1/huub

commit 4aa742b8a159aa74ba86c8a920bf183f11810707

Change the meaning of positive order literals to be `x<i`
This better matches the default `true` phase/polarity used by various
SAT solvers. The SAT solver searches by setting the earliest created
variable to `true`. However, when this literal has the meaning `x≥i`,
where `i` is close to the lower bound, this is unlikely to have much
effect on the overall problem.

This change in meaning, makes the literal representation more similar to
Chuffed.

crates/huub/src/solver/engine/int_var.rs
@@ -123,14 +123,14 @@ impl IntVar {
 			.into_iter()
 			.flatten();
 			for (ord_i, ord_j) in storage.clone().tuple_windows() {
-				let ord_i: RawLit = ord_i.into();
-				let ord_j: RawLit = ord_j.into();
-				slv.oracle.add_clause([!ord_j, ord_i]).unwrap(); // ord_j -> ord_i
+				let ord_i: RawLit = ord_i.into(); // x<i
+				let ord_j: RawLit = ord_j.into(); // x<j, where j = i + n and n≥1
+				slv.oracle.add_clause([!ord_i, ord_j]).unwrap(); // x<i -> x<(i+n)
 				if matches!(direct_encoding, DirectStorage::Eager(_)) {
 					let eq_i: RawLit = direct_enc_iter.next().unwrap().into();
-					slv.oracle.add_clause([!eq_i, ord_i]).unwrap(); // eq_i -> ord_i
-					slv.oracle.add_clause([!eq_i, !ord_j]).unwrap(); // eq_i -> !ord_j
-					slv.oracle.add_clause([eq_i, !ord_i, ord_j]).unwrap(); // !eq_i -> !ord_i \/ ord_j
+					slv.oracle.add_clause([!eq_i, !ord_i]).unwrap(); // x=i -> x≥i
+					slv.oracle.add_clause([!eq_i, ord_j]).unwrap(); // x=i -> x<(i+n)
+					slv.oracle.add_clause([eq_i, ord_i, !ord_j]).unwrap(); // x≠i -> (x<i \/ x≥(i+n))
 				}
 			}
 			debug_assert!(direct_enc_iter.next().is_none());
@@ -193,7 +193,7 @@ impl IntVar {
 			for r in self.domain.iter() {
 				let r_len = r.end() - r.start() + 1;
 				if offset < r_len {
-					return ret(LitMeaning::GreaterEq(*r.start() + offset));
+					return ret(LitMeaning::Less(*r.start() + offset));
 				}
 				offset -= r_len;
 			}
@@ -224,14 +224,14 @@ impl IntVar {
 		let mut negate = false;
 		match lit {
 			LitMeaning::Eq(i) | LitMeaning::NotEq(i) if i == lb => {
-				negate = matches!(lit, LitMeaning::Eq(_));
-				lit = LitMeaning::GreaterEq(lb + 1);
+				negate = matches!(lit, LitMeaning::NotEq(_));
+				lit = LitMeaning::Less(lb + 1);
 			}
 			LitMeaning::Eq(i) | LitMeaning::NotEq(i) if i == ub => {
-				negate = matches!(lit, LitMeaning::NotEq(_));
-				lit = LitMeaning::GreaterEq(ub);
+				negate = matches!(lit, LitMeaning::Eq(_));
+				lit = LitMeaning::Less(ub);
 			}
-			LitMeaning::Less(_) | LitMeaning::NotEq(_) => {
+			LitMeaning::GreaterEq(_) | LitMeaning::NotEq(_) => {
 				lit = !lit;
 				negate = true;
 			}
@@ -251,14 +251,14 @@ impl IntVar {
 		let (bv, negate) = self.normalize_lit_meaning(bv, lb, ub);
 
 		let bv = BoolView(match bv {
-			LitMeaning::GreaterEq(i) if i <= lb => BoolViewInner::Const(true),
-			LitMeaning::GreaterEq(i) if i > ub => BoolViewInner::Const(false),
-			LitMeaning::GreaterEq(i) => BoolViewInner::Lit(
+			LitMeaning::Less(i) if i <= lb => BoolViewInner::Const(false),
+			LitMeaning::Less(i) if i > ub => BoolViewInner::Const(true),
+			LitMeaning::Less(i) => BoolViewInner::Lit(
 				self.order_encoding
 					.entry(&self.domain, i)
 					.or_insert_with(|val, prev, next| {
 						new_var(LazyLitDef {
-							meaning: LitMeaning::GreaterEq(val),
+							meaning: LitMeaning::Less(val),
 							prev,
 							next,
 						})
@@ -313,9 +313,9 @@ impl IntVar {
 		let (bv, negate) = self.normalize_lit_meaning(bv, lb, ub);
 
 		let bv = BoolView(match bv {
-			LitMeaning::GreaterEq(i) if i <= lb => BoolViewInner::Const(true),
-			LitMeaning::GreaterEq(i) if i > ub => BoolViewInner::Const(false),
-			LitMeaning::GreaterEq(i) => self
+			LitMeaning::Less(i) if i <= lb => BoolViewInner::Const(false),
+			LitMeaning::Less(i) if i > ub => BoolViewInner::Const(true),
+			LitMeaning::Less(i) => self
 				.order_encoding
 				.find(&self.domain, i)
 				.map(|v| BoolViewInner::Lit(v.into()))?,
@@ -343,7 +343,7 @@ impl IntVar {
 		match &self.order_encoding {
 			OrderStorage::Eager { storage, .. } => {
 				let (_, offset, _) = OrderStorage::resolve_val(&self.domain, v);
-				let bv = BoolView(BoolViewInner::Lit(storage.index(offset).into()));
+				let bv = BoolView(BoolViewInner::Lit(!storage.index(offset)));
 				(bv, v)
 			}
 			OrderStorage::Lazy(storage) => {
@@ -356,7 +356,7 @@ impl IntVar {
 				};
 				while storage.storage[index].val >= v {
 					let node = &storage.storage[index];
-					let lit = BoolView(BoolViewInner::Lit(node.var.into()));
+					let lit = BoolView(BoolViewInner::Lit(!node.var));
 					if let Some(v) = trail.get_bool_val(lit) {
 						debug_assert!(v);
 						ret = (lit, node.val);
@@ -388,7 +388,7 @@ impl IntVar {
 		match &self.order_encoding {
 			OrderStorage::Eager { storage, .. } => {
 				let (_, offset, _) = OrderStorage::resolve_val(&self.domain, v);
-				let bv = BoolView(BoolViewInner::Lit(!storage.index(offset)));
+				let bv = BoolView(BoolViewInner::Lit(storage.index(offset).into()));
 				(bv, v)
 			}
 			OrderStorage::Lazy(storage) => {
@@ -401,7 +401,7 @@ impl IntVar {
 				};
 				while storage.storage[index].val <= v {
 					let node = &storage.storage[index];
-					let lit = BoolView(BoolViewInner::Lit(!node.var));
+					let lit = BoolView(BoolViewInner::Lit(node.var.into()));
 					if let Some(v) = trail.get_bool_val(lit) {
 						debug_assert!(v);
 						ret = (lit, node.val);
@@ -444,13 +444,13 @@ impl IntVar {
 					BoolViewInner::Const(true)
 				} else {
 					let (_, offset, _) = OrderStorage::resolve_val(&self.domain, lb);
-					BoolViewInner::Lit(storage.index(offset).into())
+					BoolViewInner::Lit(!storage.index(offset))
 				})
 			}
 			OrderStorage::Lazy(storage) => {
 				let lb_index = trail.get_trailed_int(storage.lb_index);
 				BoolView(if lb_index >= 0 {
-					BoolViewInner::Lit(storage[lb_index as u32].var.into())
+					BoolViewInner::Lit(!storage[lb_index as u32].var)
 				} else {
 					BoolViewInner::Const(true)
 				})
@@ -472,13 +472,13 @@ impl IntVar {
 					BoolViewInner::Const(true)
 				} else {
 					let (_, offset, _) = OrderStorage::resolve_val(&self.domain, ub + 1);
-					BoolViewInner::Lit(!storage.index(offset))
+					BoolViewInner::Lit(storage.index(offset).into())
 				})
 			}
 			OrderStorage::Lazy(storage) => {
 				let ub_index = trail.get_trailed_int(storage.ub_index);
 				BoolView(if ub_index >= 0 {
-					BoolViewInner::Lit(!storage[ub_index as u32].var)
+					BoolViewInner::Lit(storage[ub_index as u32].var.into())
 				} else {
 					BoolViewInner::Const(true)
 				})
@@ -1105,16 +1105,16 @@ impl LitMeaning {
 				let next =
 					next.expect("next should contain the GreaterEq literal for the next value"); // x≥i+n
 
-				ret.push(vec![!lit, prev]); // x=i -> x<i
-				ret.push(vec![!lit, !next]); // x=i -> x≤i+n
-				ret.push(vec![lit, !prev, next]); // x!=i -> x<i \/ x>i+n
+				ret.push(vec![!lit, !prev]); // x=i -> x≥i
+				ret.push(vec![!lit, next]); // x=i -> x<i+n
+				ret.push(vec![lit, prev, !next]); // x!=i -> x<i \/ x>i+n
 			}
-			LitMeaning::GreaterEq(_) => {
+			LitMeaning::Less(_) => {
 				if let Some(prev) = prev {
-					ret.push(vec![!lit, prev]); // x>=i -> x≥(i-n)
+					ret.push(vec![!prev, lit]); // x<(i-n) -> x<i
 				}
 				if let Some(next) = next {
-					ret.push(vec![!next, lit]); // x>=(i+n) -> x≥i
+					ret.push(vec![!lit, next]); // x<i -> x<(i+n)
 				}
 			}
 			_ => unreachable!(),
@@ -1169,24 +1169,24 @@ mod tests {
 			unreachable!()
 		};
 		let a = &mut slv.engine_mut().state.int_vars[a];
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(2)).unwrap();
-		assert_eq!(get_lit(lit), 1);
 		let lit = a.get_bool_lit(LitMeaning::Less(2)).unwrap();
+		assert_eq!(get_lit(lit), 1);
+		let lit = a.get_bool_lit(LitMeaning::GreaterEq(2)).unwrap();
 		assert_eq!(get_lit(lit), -1);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(3)).unwrap();
+		let lit = a.get_bool_lit(LitMeaning::Less(3)).unwrap();
 		assert_eq!(get_lit(lit), 2);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(4)).unwrap();
-		assert_eq!(get_lit(lit), 3);
 		let lit = a.get_bool_lit(LitMeaning::Less(4)).unwrap();
+		assert_eq!(get_lit(lit), 3);
+		let lit = a.get_bool_lit(LitMeaning::GreaterEq(4)).unwrap();
 		assert_eq!(get_lit(lit), -3);
 		let lit = a.get_bool_lit(LitMeaning::Eq(1)).unwrap();
-		assert_eq!(get_lit(lit), -1);
+		assert_eq!(get_lit(lit), 1);
 		let lit = a.get_bool_lit(LitMeaning::Eq(2)).unwrap();
 		assert_eq!(get_lit(lit), 4);
 		let lit = a.get_bool_lit(LitMeaning::Eq(3)).unwrap();
 		assert_eq!(get_lit(lit), 5);
 		let lit = a.get_bool_lit(LitMeaning::Eq(4)).unwrap();
-		assert_eq!(get_lit(lit), 3);
+		assert_eq!(get_lit(lit), -3);
 
 		let mut slv = Solver::<PropagatingCadical<_>>::from(&Cnf::default());
 		let a = IntVar::new_in(
@@ -1199,24 +1199,24 @@ mod tests {
 			unreachable!()
 		};
 		let a = &mut slv.engine_mut().state.int_vars[a];
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(2)).unwrap();
+		let lit = a.get_bool_lit(LitMeaning::Less(2)).unwrap();
 		assert_eq!(get_lit(lit), 1);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(3)).unwrap();
+		let lit = a.get_bool_lit(LitMeaning::Less(3)).unwrap();
 		assert_eq!(get_lit(lit), 2);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(4)).unwrap();
-		assert_eq!(get_lit(lit), 3);
 		let lit = a.get_bool_lit(LitMeaning::Less(4)).unwrap();
-		assert_eq!(get_lit(lit), -3);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(8)).unwrap();
 		assert_eq!(get_lit(lit), 3);
-		let lit = a.get_bool_lit(LitMeaning::Less(8)).unwrap();
+		let lit = a.get_bool_lit(LitMeaning::GreaterEq(4)).unwrap();
 		assert_eq!(get_lit(lit), -3);
-		let lit = a.get_bool_lit(LitMeaning::GreaterEq(10)).unwrap();
-		assert_eq!(get_lit(lit), 5);
+		let lit = a.get_bool_lit(LitMeaning::Less(8)).unwrap();
+		assert_eq!(get_lit(lit), 3);
+		let lit = a.get_bool_lit(LitMeaning::GreaterEq(8)).unwrap();
+		assert_eq!(get_lit(lit), -3);
 		let lit = a.get_bool_lit(LitMeaning::Less(10)).unwrap();
+		assert_eq!(get_lit(lit), 5);
+		let lit = a.get_bool_lit(LitMeaning::GreaterEq(10)).unwrap();
 		assert_eq!(get_lit(lit), -5);
 		let lit = a.get_bool_lit(LitMeaning::Eq(1)).unwrap();
-		assert_eq!(get_lit(lit), -1);
+		assert_eq!(get_lit(lit), 1);
 		let lit = a.get_bool_lit(LitMeaning::Eq(2)).unwrap();
 		assert_eq!(get_lit(lit), 6);
 		let lit = a.get_bool_lit(LitMeaning::Eq(3)).unwrap();
@@ -1224,14 +1224,14 @@ mod tests {
 		let lit = a.get_bool_lit(LitMeaning::Eq(8)).unwrap();
 		assert_eq!(get_lit(lit), 8);
 		let lit = a.get_bool_lit(LitMeaning::Eq(10)).unwrap();
-		assert_eq!(get_lit(lit), 5);
+		assert_eq!(get_lit(lit), -5);
 
 		assert_eq!(
-			a.get_bool_lit(LitMeaning::GreaterEq(1)).unwrap(),
+			a.get_bool_lit(LitMeaning::Less(11)).unwrap(),
 			BoolView(BoolViewInner::Const(true))
 		);
 		assert_eq!(
-			a.get_bool_lit(LitMeaning::Less(11)).unwrap(),
+			a.get_bool_lit(LitMeaning::GreaterEq(1)).unwrap(),
 			BoolView(BoolViewInner::Const(true))
 		);
 		assert_eq!(

crates/fzn-huub/src/trace.rs
@@ -291,7 +291,7 @@ impl RecordLazyLits {
 			let meaning = if is_eq {
 				LitMeaning::Eq
 			} else {
-				LitMeaning::GreaterEq
+				LitMeaning::Less
 			}(val);
 			let mut guard = lit_reverse_map.lock().unwrap();
 			let _ = guard.insert(lit, LitName::IntLit(iv, meaning.clone()));

crates/huub/src/solver/engine.rs
@@ -51,7 +51,7 @@ macro_rules! trace_new_lit {
 			is_eq = matches!($def.meaning, LitMeaning::Eq(_)),
 			val = match $def.meaning {
 				LitMeaning::Eq(val) => val,
-				LitMeaning::GreaterEq(val) => val,
+				LitMeaning::Less(val) => val,
 				_ => unreachable!(),
 			},
 			"register new literal"

crates/huub/src/solver/view.rs
@@ -114,9 +114,10 @@ impl IntView {
 					let _ = val_iter.next();
 					for (lit, val) in storage.clone().zip(val_iter) {
 						let i: NonZeroI32 = lit.into();
-						let geq = LitMeaning::GreaterEq(transformer.transform(val));
-						let lt = LitMeaning::Less(transformer.transform(val));
-						lits.extend([(i, geq), (-i, lt)]);
+						let orig = LitMeaning::Less(val);
+						let lt = transformer.transform_lit(orig);
+						let geq = !lt.clone();
+						lits.extend([(i, lt), (-i, geq)]);
 					}
 				}
 
@@ -126,8 +127,9 @@ impl IntView {
 					let _ = val_iter.next_back();
 					for (lit, val) in vars.clone().zip(val_iter) {
 						let i: NonZeroI32 = lit.into();
-						let eq = LitMeaning::Eq(transformer.transform(val));
-						let ne = LitMeaning::NotEq(transformer.transform(val));
+						let orig = LitMeaning::Eq(val);
+						let eq = transformer.transform_lit(orig);
+						let ne = !eq.clone();
 						lits.extend([(i, eq), (-i, ne)]);
 					}
 				}

commit 9415401f1328a61d4bccabb45c69e603f20f4cad

Fix regression caused by 4aa742b8a159aa74ba86c8a920bf183f11810707

crates/fzn-huub/benches/fzn_huub_benchmarks.rs
@@ -116,6 +116,7 @@ fn optimization(c: &mut Criterion) {
 		("jobshop_la04", &FEW_SECONDS_CONFIG),
 		("jobshop_la05", &INSTANT_CONFIG),
 		("jobshop_newspaper", &INSTANT_CONFIG),
+		("portal_10_9_10", &MILLISECONDS_CONFIG),
 		("radiation_i6_9", &INSTANT_CONFIG),
 		("radiation_i8_9", &MILLISECONDS_CONFIG),
 		("svrp_s4_v2_c3", &FEW_SECONDS_CONFIG),

crates/fzn-huub/corpus/portal_10_9_10.sol
@@ -1,0 +1,95 @@
+X_INTRODUCED_52_ = 8;
+X_INTRODUCED_53_ = 7;
+X_INTRODUCED_55_ = 8;
+X_INTRODUCED_56_ = 7;
+X_INTRODUCED_58_ = 7;
+X_INTRODUCED_59_ = 7;
+X_INTRODUCED_61_ = 6;
+X_INTRODUCED_62_ = 7;
+X_INTRODUCED_64_ = 5;
+X_INTRODUCED_65_ = 7;
+X_INTRODUCED_67_ = 4;
+X_INTRODUCED_68_ = 7;
+X_INTRODUCED_70_ = 4;
+X_INTRODUCED_71_ = 7;
+X_INTRODUCED_73_ = 4;
+X_INTRODUCED_74_ = 6;
+foundGoalAtStep = 9;
+X_INTRODUCED_458_ = 0;
+X_INTRODUCED_465_ = 0;
+X_INTRODUCED_473_ = 0;
+X_INTRODUCED_480_ = 0;
+X_INTRODUCED_496_ = false;
+X_INTRODUCED_497_ = 0;
+X_INTRODUCED_504_ = false;
+X_INTRODUCED_505_ = 0;
+X_INTRODUCED_517_ = false;
+X_INTRODUCED_518_ = 0;
+X_INTRODUCED_525_ = false;
+X_INTRODUCED_526_ = 0;
+X_INTRODUCED_541_ = false;
+X_INTRODUCED_542_ = 0;
+X_INTRODUCED_549_ = false;
+X_INTRODUCED_550_ = 0;
+X_INTRODUCED_561_ = false;
+X_INTRODUCED_562_ = 0;
+X_INTRODUCED_569_ = false;
+X_INTRODUCED_570_ = 0;
+X_INTRODUCED_584_ = false;
+X_INTRODUCED_585_ = 0;
+X_INTRODUCED_592_ = false;
+X_INTRODUCED_593_ = 0;
+X_INTRODUCED_604_ = false;
+X_INTRODUCED_605_ = 0;
+X_INTRODUCED_612_ = false;
+X_INTRODUCED_613_ = 0;
+X_INTRODUCED_627_ = false;
+X_INTRODUCED_628_ = 0;
+X_INTRODUCED_635_ = false;
+X_INTRODUCED_636_ = 0;
+X_INTRODUCED_647_ = false;
+X_INTRODUCED_648_ = 0;
+X_INTRODUCED_655_ = false;
+X_INTRODUCED_656_ = 0;
+X_INTRODUCED_670_ = false;
+X_INTRODUCED_671_ = 0;
+X_INTRODUCED_678_ = false;
+X_INTRODUCED_679_ = 0;
+X_INTRODUCED_690_ = false;
+X_INTRODUCED_691_ = 0;
+X_INTRODUCED_698_ = false;
+X_INTRODUCED_699_ = 0;
+X_INTRODUCED_713_ = false;
+X_INTRODUCED_714_ = 0;
+X_INTRODUCED_721_ = false;
+X_INTRODUCED_722_ = 0;
+X_INTRODUCED_733_ = false;
+X_INTRODUCED_734_ = 0;
+X_INTRODUCED_741_ = false;
+X_INTRODUCED_742_ = 0;
+X_INTRODUCED_756_ = false;
+X_INTRODUCED_757_ = 0;
+X_INTRODUCED_764_ = false;
+X_INTRODUCED_765_ = 0;
+X_INTRODUCED_776_ = false;
+X_INTRODUCED_777_ = 0;
+X_INTRODUCED_784_ = false;
+X_INTRODUCED_785_ = 0;
+X_INTRODUCED_799_ = false;
+X_INTRODUCED_800_ = 0;
+X_INTRODUCED_807_ = false;
+X_INTRODUCED_808_ = 0;
+X_INTRODUCED_819_ = false;
+X_INTRODUCED_820_ = 0;
+X_INTRODUCED_827_ = false;
+X_INTRODUCED_828_ = 0;
+X_INTRODUCED_842_ = false;
+X_INTRODUCED_843_ = 0;
+X_INTRODUCED_850_ = false;
+X_INTRODUCED_851_ = 0;
+X_INTRODUCED_862_ = false;
+X_INTRODUCED_863_ = 0;
+X_INTRODUCED_870_ = false;
+X_INTRODUCED_871_ = 0;
+playerHeading = [1,1,4,4,4,4,4,1,1,4];
+playerAction = [1,2,1,1,1,1,3,1,2,2];

crates/fzn-huub/corpus/recompile_mzn.py
@@ -28,6 +28,13 @@ INSTANCES = [
             "jobshop_newspaper.fzn.json": "https://gist.githubusercontent.com/Dekker1/6662819320b5741b57913813dfcc0afa/raw/11e882546aed599a33a7c9426a445b8d0ac986c7/jobshop_newspaper.dzn",
         },
     },
+    {
+        # Portal
+        "model": "https://raw.githubusercontent.com/MiniZinc/mzn-challenge/refs/heads/develop/2024/portal/portal.mzn",
+        "instances": {
+            "portal_10_9_10.fzn.json": "https://raw.githubusercontent.com/MiniZinc/mzn-challenge/refs/heads/develop/2024/portal/random_10_9_10.json"
+        }
+    },
     {  # Radiation
         "model": "https://raw.githubusercontent.com/MiniZinc/mzn-challenge/refs/heads/develop/2020/radiation/radiation.mzn",
         "instances": {

crates/huub/src/solver/engine/int_var.rs
@@ -76,6 +76,7 @@ pub(crate) struct IntVar {
 	upper_bound: TrailedInt,
 }
 
+#[derive(Debug)]
 /// The definition given to a lazily created literal.
 pub(crate) struct LazyLitDef {
 	/// The meaning that the literal is meant to represent.
@@ -354,7 +355,7 @@ impl IntVar {
 					let (entry, prev) = self.order_encoding.entry(&self.domain, i).or_insert_with(
 						|val, prev, next| {
 							new_var(LazyLitDef {
-								meaning: LitMeaning::GreaterEq(val),
+								meaning: LitMeaning::Less(val),
 								prev,
 								next,
 							})
@@ -364,7 +365,7 @@ impl IntVar {
 						.next_value()
 						.or_insert_with(|val, prev, next| {
 							new_var(LazyLitDef {
-								meaning: LitMeaning::GreaterEq(val),
+								meaning: LitMeaning::Less(val),
 								prev,
 								next,
 							})